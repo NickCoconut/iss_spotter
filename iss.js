a4caa90eafd47be45dac19c371de5798bf739b24
NickCoconut <gelaxyy@gmail.com>
2021-12-03 03:47:16 +0000

    addFile

--- iss.js
+++ iss.js
@@ -1,1 +1,84 @@
+const request = require('request');
 
+const fetchMyIP = (callback) => {
+const domain = `https://api.ipify.org/?format=json`;
+request (domain, (error, response, body) => {
+  if (error) {
+  callback(error, null);
+  return;
+}
+
+  if (response.statusCode !== 200) {
+  const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+  callback(Error(msg), null);
+  return;
+}
+
+  const data = JSON.parse(body)
+  callback (null, data.ip)
+
+})
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+
+module.exports = { nextISSTimesForMyLocation};
